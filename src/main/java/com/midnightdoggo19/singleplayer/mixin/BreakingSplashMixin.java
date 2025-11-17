aa10a1c7f1533a6c3c02927dfb0692611a8c9a4d
Midnight Doggo <git@midnightdoggo19.com>
2025-11-17 22:42:58 +0000

    Don't splash

---
+++ src/main/java/com/midnightdoggo19/singleplayer/mixin/BreakingSplashMixin.java
@@ -0,0 +1,17 @@
+package com.midnightdoggo19.singleplayer.mixin;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.SplashTextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(SplashTextRenderer.class)
+public class BreakingSplashMixin {
+    /**
+     * @author Midnight Doggo
+     * @reason Breaking a splash.
+     */
+    @Overwrite
+    public void render (DrawContext context, int screenWidth, TextRenderer textRenderer, float alpha) {}
+}

5c16431cf3bd22223c9b858e6384f105eaba67be
Midnight Doggo <git@midnightdoggo19.com>
2025-11-18 01:13:42 +0000

    got rid of the text at the bottom of the screen

--- src/main/java/com/midnightdoggo19/singleplayer/mixin/TitleMixin.java
+++ src/main/java/com/midnightdoggo19/singleplayer/mixin/TitleMixin.java
@@ -1,11 +1,16 @@
 package com.midnightdoggo19.singleplayer.mixin;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleMixin
@@ -29,6 +34,27 @@ public class TitleMixin
         );
         return y;
     }
+
+    /**
+     * @param context is ignored.
+     * @param mouseX is ignored.
+     * @param mouseY is ignored.
+     * @param deltaTicks is ignored.
+     * @param ci is ignored.
+     * This exists to shove text away.
+     */
+    @Inject(method = "render", at = @At("HEAD"))
+    public void shove (DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci) {
+        this.height = this.height * 2; // this is so scuffed but it forces the test off the screen
+    }
+
+    /**
+     * Gets rid of the copyright field in the bottom right.
+     */
+    @Shadow
+    private static final Text COPYRIGHT = Text.literal("");
+
+    @Shadow @Final private static Logger LOGGER;
 
     protected TitleMixin(Text title) {
         super(title);

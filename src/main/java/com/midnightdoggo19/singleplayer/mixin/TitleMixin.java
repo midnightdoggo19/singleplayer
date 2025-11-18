e7d7344554c04e98cff8098f6d801b81d53c482a
Midnight Doggo <git@midnightdoggo19.com>
2025-11-18 19:03:18 +0000

    removed unused import

--- src/main/java/com/midnightdoggo19/singleplayer/mixin/TitleMixin.java
+++ src/main/java/com/midnightdoggo19/singleplayer/mixin/TitleMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
